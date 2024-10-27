e9a0bb496608a9a70b96d73eee759d97873c400e
Michael Tryfanau <michael.tryfanau@gmail.com>
2024-10-27 17:33:21 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -6,7 +6,30 @@ namespace Simulator
 {
     public class Animals
     {
-        public required string Description { get; init; }
+        private string description = "Capybaras";
+        private uint size;
+        public required string Description
+        {
+            get => description;
+            init
+            {
+                value = value.Trim();
+                if (value.Length > 15)
+                {
+                    value = value.Substring(0, 15);
+                }
+                value = value.TrimEnd();
+                if (value.Length < 3)
+                {
+                    value += new String('#', 3 - value.Length);
+                }
+                if (value[0] is >= 'a' and <= 'z')
+                {
+                    value = (char)(value[0] - 32) + value.Substring(1);
+                }
+                description = value;
+            }
+        }
         public uint Size { get; set; } = 3;
         public string Info
         {
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -4,19 +4,55 @@ namespace Simulator
 {
     public class Creature
     {
-        private readonly string? name;
+        private string name = "Unknown";
         private int level;
-        public Creature()
-        {
-            
-        }
+        public Creature() { }
         public Creature(string name, int level=1)
         {
             Name = name;
             Level = level;
         }
-        public string Name { get; init; }
-        public int Level { get; set; }
+        public string Name
+        {
+            get => name;
+            init
+            {
+                value = value.Trim();
+                if (value.Length > 25)
+                {
+                    value = value.Substring(0, 25);
+                }
+                value = value.TrimEnd();
+                if (value.Length < 3)
+                {
+                    value += new String('#', 3 - value.Length);
+                }
+                if (value[0] is >= 'a' and <= 'z')
+                {
+                    value = (char)(value[0] - 32) + value.Substring(1);
+                }
+                name = value;
+            }
+        }
+        public int Level
+        {
+            get => level;
+            init
+            {
+                if (value < 1)
+                {
+                    level = 1;
+                }
+                else if (value > 10)
+                {
+                     level = 10;
+                }
+                else
+                {
+                    level = value;
+                }
+            }
+        }
         public string Info
         {
             get { return $"{Name} [{Level}]"; }
@@ -24,6 +60,13 @@ namespace Simulator
         public void SayHi()
         {
             Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        }
+        public void Upgrade()
+        {
+            if (level<10)
+            {
+                level++;
+            }
         }
     }
 }
