58899ece1e20450dece40a25e9e4ff354c88b10b
Michael Tryfanau <michael.tryfanau@gmail.com>
2024-10-27 16:16:21 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Simulator
+{
+    public class Animals
+    {
+        public required string Description { get; init; }
+        public uint Size { get; set; } = 3;
+        public string Info
+        {
+            get { return $"{Description} <{Size}>"; }
+        }
+    }
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Simulator
+{
+    public class Creature
+    {
+        private readonly string? name;
+        private int level;
+        public Creature()
+        {
+            
+        }
+        public Creature(string name, int level=1)
+        {
+            Name = name;
+            Level = level;
+        }
+        public string Name { get; init; }
+        public int Level { get; set; }
+        public string Info
+        {
+            get { return $"{Name} [{Level}]"; }
+        }
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        }
+    }
+}
+
