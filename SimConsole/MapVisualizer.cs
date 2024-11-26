52ecae2250d098bcb3046e15931498f3b0e50098
Michael Tryfanau <michael.tryfanau@gmail.com>
2024-11-27 00:52:27 +0100

    SimConsole

---
+++ SimConsole/MapVisualizer.cs
@@ -0,0 +1,93 @@
+﻿using Simulator;
+using Simulator.Maps;
+using System.Diagnostics.Tracing;
+namespace SimConsole;
+
+public static class Box
+{
+    public const char
+        Horizontal = '\u2500',
+        Vertical = '\u2502',
+        Cross = '\u253C',
+
+        TopLeft = '\u250C',
+        TopRight = '\u2510',
+        TopMid = '\u252C',
+
+        BottomLeft = '\u2514',
+        BottomMid = '\u2534',
+        BottomRight = '\u2518',
+
+        MidLeft = '\u251C',
+        MidRight = '\u2524';
+}
+
+public class MapVisualizer
+{
+    private Map map;
+    public MapVisualizer(Map map)
+    {
+        this.map = map;
+    }
+    public void Visualize()
+    {
+        //Upper boundary
+        Console.Write(Box.TopLeft);
+        Console.Write(Box.Horizontal);
+        for (int i = 1; i < map.SizeX; i++)
+        {
+            Console.Write(Box.TopMid);
+            Console.Write(Box.Horizontal);
+        }
+        Console.WriteLine(Box.TopRight);
+        //Data
+        for (int i = map.SizeY-1; i >=0; i--)
+        {
+            for (int j = 0; j < map.SizeX; j++)
+            {
+                Console.Write(Box.Vertical);
+                List<Creature> field = map.At(j, i);
+                if (field.Count == 1)
+                {
+                    if (field.ElementAt(0) is Elf)
+                    {
+                        Console.Write('E');
+                    }
+                    else
+                    {
+                        Console.Write('O');
+                    }
+                }
+                else if (field.Count > 1)
+                {
+                    Console.Write('X');
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine(Box.Vertical);
+            if (i > 0)
+            {
+                Console.Write(Box.MidLeft);
+                Console.Write(Box.Horizontal);
+                for (int j = 1; j < map.SizeX; j++)
+                {
+                    Console.Write(Box.Cross);
+                    Console.Write(Box.Horizontal);
+                }
+                Console.WriteLine(Box.MidRight);
+            }
+        }
+        //Bottom
+        Console.Write(Box.BottomLeft);
+        Console.Write(Box.Horizontal);
+        for (int i = 1; i < map.SizeY; i++)
+        {
+            Console.Write(Box.BottomMid);
+            Console.Write(Box.Horizontal);
+        }
+        Console.WriteLine(Box.BottomRight);
+    }
+}
---
+++ SimConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using Simulator.Maps;
+using Simulator;
+
+namespace SimConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        SmallSquareMap map = new(5);
+        List<Creature> creatures = [new Orc("Gorbag"), new Elf("Elandor")];
+        List<Point> points = [new(2, 2), new(3, 1)];
+        string moves = "dlrludl";
+
+        Simulation simulation = new(map, creatures, points, moves);
+        MapVisualizer mapVisualizer = new(simulation.Map);
+        Console.WriteLine("SIMULATION!\n\nStarting positions:");
+        mapVisualizer.Visualize();
+        int turn = 1;
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.WriteLine();
+                simulation.Turn();
+                Console.WriteLine($"Turn {turn}:");
+                Console.WriteLine($"{simulation.CurrentCreature.GetType().Name.ToString().ToUpper()}: {simulation.CurrentCreature.Info} {simulation.CurrentCreature.Position} goes {simulation.CurrentMoveName}:");
+                mapVisualizer.Visualize();
+                turn++;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                break;
+            }
+        }
+    }
+}
