b364c7285252a570b0082897a8e3f70ae21238d0
Michael Tryfanau <michael.tryfanau@gmail.com>
2024-12-30 12:51:45 +0100

    Sort

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -3,130 +3,61 @@ using Simulator.Maps;
 
 internal class Program
 {
-    private static void Lab5a()
+    private static int CompareCreaturesByLevel(Creature c1, Creature c2)
     {
-        Point p1 = new(10, 25);
-        Point p2 = new(-4, 3);
-        try
-        {
-            Console.Write("r1: ");
-            Rectangle r1 = new(-1, -8, -4, -8);
-            Console.WriteLine(r1);
-            Console.WriteLine($"Is p1{p1} in r1({r1})? {(r1.Contains(p1) ? "Yes" : "No")}.");
-            Console.WriteLine($"Is p2{p2} in r1({r1})? {(r1.Contains(p2) ? "Yes" : "No")}.\n");
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
-        try
-        {
-            Console.Write("r2: ");
-            Rectangle r2 = new(new Point(1, 3), new Point(11, 43));
-            Console.WriteLine(r2);
-            Console.WriteLine($"Is p1{p1} in r2({r2})? {(r2.Contains(p1) ? "Yes" : "No")}.");
-            Console.WriteLine($"Is p2{p2} in r2({r2})? {(r2.Contains(p2) ? "Yes" : "No")}.\n");
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
-        try
-        {
-            Console.Write("r3: ");
-            Rectangle r3 = new(1, 5, 4, 0);
-            Console.WriteLine(r3);
-            Console.WriteLine($"Is p1{p1} in r3({r3})? {(r3.Contains(p1) ? "Yes" : "No")}.");
-            Console.WriteLine($"Is p2{p2} in r3({r3})? {(r3.Contains(p2) ? "Yes" : "No")}.\n");
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
-        try
-        {
-            Console.Write("r4: ");
-            Rectangle r4 = new(50, 19, -4, -100);
-            Console.WriteLine(r4);
-            Console.WriteLine($"Is p1{p1} in r4({r4})? {(r4.Contains(p1) ? "Yes" : "No")}.");
-            Console.WriteLine($"Is p2{p2} in r4({r4})? {(r4.Contains(p2) ? "Yes" : "No")}.\n");
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
+        return c1.Level.CompareTo(c2.Level);
     }
-    private static void Lab5b()
+    private static int CompareCreaturesByPower(Creature c1, Creature c2)
     {
-        Point[] points = {new(10, 15), new(-4, 3), new(8, 1), new(0, 9), new(7, 19), new(4, 0), new (7, 7)};
-        try
-        {
-            Console.WriteLine("m1:");
-            SmallSquareMap m1 = new(4);
-            Console.WriteLine("Map created!");
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
-        try
-        {
-            Console.WriteLine("m2:");
-            SmallSquareMap m2 = new(24);
-            Console.WriteLine("Map created!");
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
-        try
-        {
-            Console.WriteLine("m3:");
-            SmallSquareMap m3 = new(20);
-            Console.WriteLine("Map created!");
-            foreach (var point in points)
-            {
-                Console.WriteLine($"{point} {(m3.Exist(point) ? "exists" : "doesn't exist")} in m3.");
-            }
-            Console.WriteLine();
-            Console.WriteLine($"{points[0]} upwards: {m3.Next(points[0], Direction.Up)}");
-            Console.WriteLine($"{points[2]} to the right: {m3.Next(points[2], Direction.Right)}");
-            Console.WriteLine($"{points[3]} to the left: {m3.Next(points[3], Direction.Left)}");
-            Console.WriteLine($"{points[3]} to the southwest: {m3.NextDiagonal(points[3], Direction.Down)}");
-            Console.WriteLine($"{points[4]} to the northeast: {m3.NextDiagonal(points[4], Direction.Up)}");
-            Console.WriteLine($"{points[5]} to the southeast: {m3.NextDiagonal(points[5], Direction.Right)}");
-            Console.WriteLine();
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
-        try
-        {
-            Console.WriteLine("m4:");
-            SmallSquareMap m4 = new(8);
-            Console.WriteLine("Map created!");
-            foreach (var point in points)
-            {
-                Console.WriteLine($"{point} {(m4.Exist(point) ? "exists" : "doesn't exist")} in m4.");
-            }
-            Console.WriteLine();
-            Console.WriteLine($"{points[5]} upwards: {m4.Next(points[5], Direction.Up)}");
-            Console.WriteLine($"{points[5]} to the southwest: {m4.NextDiagonal(points[5], Direction.Down)}");
-            Console.WriteLine($"{points[6]} to the southwest: {m4.NextDiagonal(points[6], Direction.Down)}");
-            Console.WriteLine($"{points[6]} to the northwest: {m4.NextDiagonal(points[6], Direction.Left)}");
-            Console.WriteLine($"{points[6]} to the left: {m4.Next(points[6], Direction.Left)}");
-            Console.WriteLine();
-        }
-        catch
-        {
-            Console.WriteLine("An error occurred.\n");
-        }
+        return c1.Power.CompareTo(c2.Power);
+    }
+    private static int OverableCreaturesComparison(Creature c1, Creature c2)
+    {
+        double overabilityC1 = c1 is Elf ? ((Elf)c1).Agility / c1.Level : ((Orc)c1).Rage / c1.Level;
+        double overabilityC2 = c2 is Elf ? ((Elf)c2).Agility / c2.Level : ((Orc)c2).Rage / c2.Level;
+        return overabilityC1.CompareTo(overabilityC2)*(-1);
     }
     static void Main(string[] args)
     {
-        Console.WriteLine("Starting Simulator!\n");
-        //Lab5a();
-        Lab5b();
+        List<Creature> creatures = [new Orc("Gorbag", rage: 2, level: 3), new Elf("Elandor"),
+            new Orc("Szambag", level: 4, rage: 25), new Elf("Szambandor", agility: 7),
+            new Orc("Marian", rage: 2, level: -10), new Elf("Misha Sha", level: 9, agility: 6),
+            new Orc("Kharton", level: 3), new Elf("Van Darkholme", level: 6, agility: 10),
+            new Orc("Donald", rage: 200, level: -3), new Elf("Joe", level: 3, agility: 0),
+            new Orc("Jarosław Kaczyński", level: 5, rage: 8), new Elf("Alexander Zudin", agility: 7)];
+
+        Console.WriteLine("Unsorted list of creatures:");
+        foreach (var creature in creatures)
+        {
+            Console.WriteLine(creature.GetType().Name.ToString().ToUpper() + ": " + creature.Info);
+        }
+
+        creatures.Sort(CompareCreaturesByPower);
+        Console.WriteLine("\nList of creatures sorted by Power:");
+        foreach (var creature in creatures)
+        {
+            Console.WriteLine(creature.GetType().Name.ToString().ToUpper() + ": " + creature.Info);
+        }
+
+        creatures.Sort(CompareCreaturesByLevel);
+        Console.WriteLine("\nList of creatures sorted by Level:");
+        foreach (var creature in creatures)
+        {
+            Console.WriteLine(creature.GetType().Name.ToString().ToUpper() + ": " + creature.Info);
+        }
+
+        creatures.Sort((x, y) => { return x.Name.CompareTo(y.Name); });
+        Console.WriteLine("\nList of creatures sorted by Name:");
+        foreach (var creature in creatures)
+        {
+            Console.WriteLine(creature.GetType().Name.ToString().ToUpper() + ": " + creature.Info);
+        }
+
+        creatures.Sort(OverableCreaturesComparison);
+        Console.WriteLine("\nList of creatures sorted by Overability, descendingly:");
+        foreach (var creature in creatures)
+        {
+            Console.WriteLine(creature.GetType().Name.ToString().ToUpper() + ": " + creature.Info);
+        }
     }
 }
